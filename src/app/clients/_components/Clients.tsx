481387225275a1b6b4460b77d236c60501704f4a
sukemaru-injp <sukemaro_0@yahoo.co.jp>
2024-09-10 17:50:03 +0900

    test

--- src/app/clients/_components/Clients.tsx
+++ src/app/clients/_components/Clients.tsx
@@ -6,5 +6,8 @@ export const Clients = async ({ render }: Props) => {
 	const clients = await fincode.customers.retrieveList(undefined, {
 		tenantShopId: process.env.NEXT_PUBLIC_FINCODE_TENANT_TARGET_ID ?? "",
 	});
+	// 一括登録された決済を取得
+	// const test = await fincode.paymentBulks.retrieveDetailList("", { pay_type: "Card" });
+	// console.log("test", test);
 	return render(clients);
 };
